efce11df440cbb3bf1b619cfaaa720a1bd7f53bb
anjalikhetan <anjali.khetan27@gmail.com>
2015-03-31 01:04:05 -0400

    added homepage view
    outline of homepage

--- Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -9,12 +9,53 @@ package com.parse.starter;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleColor=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleDimension=0x7f010001;
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static final int exampleDrawable=0x7f010003;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int exampleString=0x7f010000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button=0x7f050004;
+        public static final int button2=0x7f050003;
+        public static final int button3=0x7f050002;
+        public static final int listView=0x7f050000;
+        public static final int textView=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int sample_homepage_view=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -22,4 +63,82 @@ public final class R {
         public static final int parse_app_id=0x7f040002;
         public static final int parse_client_key=0x7f040003;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a HomepageView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #HomepageView_exampleColor com.parse.starter:exampleColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #HomepageView_exampleDimension com.parse.starter:exampleDimension}</code></td><td></td></tr>
+           <tr><td><code>{@link #HomepageView_exampleDrawable com.parse.starter:exampleDrawable}</code></td><td></td></tr>
+           <tr><td><code>{@link #HomepageView_exampleString com.parse.starter:exampleString}</code></td><td></td></tr>
+           </table>
+           @see #HomepageView_exampleColor
+           @see #HomepageView_exampleDimension
+           @see #HomepageView_exampleDrawable
+           @see #HomepageView_exampleString
+         */
+        public static final int[] HomepageView = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.parse.starter.R.attr#exampleColor}
+          attribute's value can be found in the {@link #HomepageView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.parse.starter:exampleColor
+        */
+        public static final int HomepageView_exampleColor = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.parse.starter.R.attr#exampleDimension}
+          attribute's value can be found in the {@link #HomepageView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.parse.starter:exampleDimension
+        */
+        public static final int HomepageView_exampleDimension = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.parse.starter.R.attr#exampleDrawable}
+          attribute's value can be found in the {@link #HomepageView} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name com.parse.starter:exampleDrawable
+        */
+        public static final int HomepageView_exampleDrawable = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.parse.starter.R.attr#exampleString}
+          attribute's value can be found in the {@link #HomepageView} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.parse.starter:exampleString
+        */
+        public static final int HomepageView_exampleString = 0;
+    };
 }
