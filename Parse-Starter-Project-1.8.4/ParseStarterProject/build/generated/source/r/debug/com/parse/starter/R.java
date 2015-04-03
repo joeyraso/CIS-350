22984b943294639fa5d7c5f03302c37cd13b1655
anjalikhetan <anjali.khetan27@gmail.com>
2015-04-03 04:57:42 -0400

    Dashboard list
    loading in objects from parse. have not displayed yet.

--- Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -120,30 +120,31 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090018;
+        public static final int action_settings=0x7f090019;
         public static final int button3=0x7f090009;
-        public static final int emailText=0x7f090014;
+        public static final int emailText=0x7f090015;
         public static final int emailTextView=0x7f090003;
-        public static final int listView=0x7f090006;
+        public static final int list=0x7f090006;
         public static final int listview=0x7f090000;
         public static final int logOutButton=0x7f090005;
-        public static final int passwordSignIn=0x7f09000e;
-        public static final int passwordText=0x7f090012;
+        public static final int passwordSignIn=0x7f09000f;
+        public static final int passwordText=0x7f090013;
         public static final int phoneNumberTextView=0x7f090004;
-        public static final int phoneText=0x7f090016;
+        public static final int phoneText=0x7f090017;
         public static final int profileButton=0x7f090008;
-        public static final int signinButton=0x7f09000f;
+        public static final int signinButton=0x7f090010;
         public static final int signupButton=0x7f09000a;
-        public static final int signupButton2=0x7f090017;
+        public static final int signupButton2=0x7f090018;
         public static final int textView=0x7f090007;
         public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f09000b;
-        public static final int textView3=0x7f09000d;
-        public static final int textView4=0x7f090011;
-        public static final int textView5=0x7f090013;
-        public static final int textView6=0x7f090015;
-        public static final int usernameSignIn=0x7f09000c;
-        public static final int usernameText=0x7f090010;
+        public static final int textView2=0x7f09000c;
+        public static final int textView3=0x7f09000e;
+        public static final int textView4=0x7f090012;
+        public static final int textView5=0x7f090014;
+        public static final int textView6=0x7f090016;
+        public static final int textView7=0x7f09000b;
+        public static final int usernameSignIn=0x7f09000d;
+        public static final int usernameText=0x7f090011;
         public static final int usernameTextView=0x7f090002;
     }
     public static final class layout {
--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/HomepageActivity.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/HomepageActivity.java
@@ -6,7 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.app.ListActivity;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
 
 public class HomepageActivity extends Activity {
 
@@ -14,6 +29,46 @@ public class HomepageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_homepage_view);
+
+        //Set up listview
+        final ArrayList<String> jobNames = new ArrayList<String>();
+        final ArrayList<String> jobDescriptions = new ArrayList<String>();
+        //populate
+        ListView jobslv = (ListView) findViewById(android.R.id.list);
+//        jobslv.setAdapter(listAdapter);
+
+        //Querey Parse
+        ParseQuery query = new ParseQuery("Job");
+        query.findInBackground(new FindCallback() {
+            @Override
+            public void done(List objects, ParseException e) {
+                for (int i = 0; i < objects.size(); i++) {
+                    ParseObject o = (ParseObject)objects.get(i);
+                    String name = o.getString("jobName");
+                    String descr = o.getString("jobDescription");
+                    jobNames.add(name);
+                    jobDescriptions.add(descr);
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Hello toast!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+//                    listAdapter.add(name);
+//                    listAdapter.add(descr);
+                }
+            }
+
+            @Override
+            public void done(Object o, Throwable throwable) {
+
+            }
+        });
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, jobNames);
+//        jobslv.setAdapter(adapter);
     }
 
 
