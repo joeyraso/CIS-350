b21944b657095b4918a0337dae2a93a0f487836b
anjalikhetan <anjali.khetan27@gmail.com>
2015-04-15 18:08:25 -0400

    Adds functionality for job request button
    updates the Job and the User to reflect this request.

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/JobDetailsActivity.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/JobDetailsActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.util.Log;
 
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.GetCallback;
+import com.parse.ParseUser;
+
 import android.widget.TextView;
 
 import java.util.List;
@@ -22,19 +26,21 @@ import java.util.List;
 
 public class JobDetailsActivity extends Activity {
     Job job;
+    String jobId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_details);
 
         Intent intent = getIntent();
-        String jobId = intent.getStringExtra("jobID");
+        jobId = intent.getStringExtra("jobID");
 
         //Query Parse
         ParseQuery<Job> query = new ParseQuery("Job");
         query.getInBackground(jobId, new GetCallback<Job>() {
             @Override
             public void done(Job o, ParseException e) {
+                job = o;
                 TextView jobNameTextObject = (TextView) findViewById(R.id.detailsName);
                 TextView jobDescriptionTextObject = (TextView) findViewById(R.id.detailsDescription);
                 TextView startDateTextObject = (TextView) findViewById(R.id.detailsStartDate);
@@ -70,5 +76,45 @@ public class JobDetailsActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void requestJob(View view) {
+        //Update both the User and the Jobs
+        addJobToMyRequested(); //current user gets this job added to requests
+        addUserToJobRequestors();//add current user to jobs list of requestors
+
+
+        Intent intent = new Intent(this, CartActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void addJobToMyRequested() {
+        //Copy over the old list of reqeustedJobs
+        String[] currRequests = (String[]) ParseUser.getCurrentUser().get("myRequestedJobs");
+        String[] newRequests = new String[currRequests.length + 1];
+        for (int i = 0; i < currRequests.length; i++) {
+            newRequests[i] = currRequests[i];
+        }
+
+        //This adds the requested job to the list of requestedJobs
+        newRequests[newRequests.length - 1] = jobId;
+
+        //update so the User contains the new list of requestedJobs
+        ParseUser.getCurrentUser().put("myRequestedJobs", newRequests);
+    }
+
+    private void addUserToJobRequestors() {
+        String[] currRequestors = (String[]) job.get("jobRequestors");
+        String[] newRequestors = new String[currRequestors.length + 1];
+        for (int i = 0; i < currRequestors.length; i++) {
+            newRequestors[i] = currRequestors[i];
+        }
+
+        //This adds the user to the list of jobRequestors
+        newRequestors[newRequestors.length - 1] = ParseUser.getCurrentUser().getObjectId();
+
+        //update so the Job contains the new requesotr
+       job.put("jobRequestors", newRequestors);
     }
 }
