e820ada87e39adce11a6bf6cbd0ae49c72ef8b73
Ankita <ankitachadha07@gmail.com>
2015-04-16 01:43:50 -0400

    MyPostedJobs
    This changed the bottom tabs to include “Add Job” and adds some
    functionality to MyPostedJobs.
    Known bug: when we add to myPostedJobs list (does not deal with) and
    commented out.

--- Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/jobCreationActivity.java
+++ Parse-Starter-Project-1.8.4/ParseStarterProject/src/com/parse/starter/jobCreationActivity.java
@@ -1,6 +1,5 @@
 package com.parse.starter;
 
-import com.parse.ParseObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +9,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class jobCreationActivity extends Activity {
 
@@ -93,7 +94,40 @@ public class jobCreationActivity extends Activity {
         Job newJob = new Job(jobName, jobDescription, startDate, endDate);
         newJob.saveInBackground();
 
+
+        // adds to MyPostedJobs list
+        String newJobId = newJob.getObjectId();
+
+
+        /*ParseQuery<Job> query = new ParseQuery("Job");
+        query.getInBackground(newJob.getObjectId(), new GetCallback<Job>() {
+            @Override
+            public void done(Job o, ParseException e) {
+                 jobId = o.getObjectId();
+            }
+        });*/
+
+
+
+        addJobToMyPostedJobs(newJobId);
+
         Intent intent = new Intent(this, HomepageActivity.class);
         startActivity(intent);
     }
+
+
+    // add the job to the user's list of myPostedJobs
+    public void addJobToMyPostedJobs(String newJobId) {
+        //Updates myPostedJobs list in the User
+        List<String> myPostedJobs = ParseUser.getCurrentUser().getList("myPostedJobs");
+        if (myPostedJobs == null) {
+            ParseUser.getCurrentUser().put("myPostedJobs", new ArrayList<String>());
+            myPostedJobs = ParseUser.getCurrentUser().getList("myPostedJobs");
+        }
+
+        myPostedJobs.add(newJobId);
+        ParseUser.getCurrentUser().saveInBackground();
+        return;
+    }
+
 }
